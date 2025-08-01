4018e649e621f0161ac2301c4bd33e6306df7a5e
Maikel Arndt <98112836+Newkoop@users.noreply.github.com>
2025-08-01 19:21:53 +0200

    fix in main and relocation of files

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,33 @@
-import mongoose from 'mongoose';
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import path from "path";
 
-mongoose.connect('mongodb://localhost:27017/myapp');
 const app = express();
-app.use(cors());
+const PORT = process.env.PORT || 5000;
+
+const corsOptions = {
+  origin: "http://localhost:5173",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
-app.listen(3000, () => {
-console.log('Server running on port 3000');
-});
+
+mongoose
+  .connect("mongodb://localhost:27017/DoLearning", {
+    serverSelectionTimeoutMS: 5000,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error(err));
+
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+
+
+app.use("/images", express.static(path.join(__dirname, "assets", "thumbs")));
+app.use("/posters", express.static(path.join(__dirname, "assets", "poster")));
+
